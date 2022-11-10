d281810695d3ff2846ec309850123ad164734d99
Melvin W <melvinhw@me.com>
2022-11-10 11:19:52 +0000

    Initial Commit

---
+++ AuthenticationDemo/AuthenticationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationDemoApp.swift
+//  AuthenticationDemo
+//
+//  Created by M W on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
